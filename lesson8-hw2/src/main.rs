09e23c937775ccc01e56902cf095f1f6403d2376
Alex Grachev <alex.f.grach@gmail.com>
2025-05-04 17:11:14 +0000

    example report generation in main added

--- lesson8-hw2/src/main.rs
+++ lesson8-hw2/src/main.rs
@@ -1,49 +1,48 @@
-use rand::Rng;
+use std::collections::HashMap;
+
+use lesson8_hw2::{
+    report::print_report,
+    smarthome::{Room, SmartDevice, SmartHouse, Socket, Thermometer},
+};
+use rand::{self, rngs::ThreadRng};
 
 fn main() {
-    //     let rnd = rand::rng();
+    let rnd = rand::rng();
 
-    //     let mut rooms: Vec<Box<dyn AnyRoom<_>>> = Vec::new();
+    let room1: Room<ThreadRng> = Room::new(HashMap::<String, SmartDevice<ThreadRng>>::from([
+        ("s1".to_owned(), Socket::new("Europe", rnd.clone()).into()),
+        (
+            "t1".to_owned(),
+            Thermometer::new("Celsius", rnd.clone()).into(),
+        ),
+    ]));
 
-    //     let room1 = Room::new(
-    //         123,
-    //         [
-    //             Socket::new(123, rnd.clone()).into(),
-    //             Thermometer::new(345, rnd.clone()).into(),
-    //         ],
-    //     );
+    let room2: Room<ThreadRng> = Room::new(HashMap::<String, SmartDevice<ThreadRng>>::from([
+        ("s1".to_owned(), Socket::new("Europe", rnd.clone()).into()),
+        ("s2".to_owned(), Socket::new("Europe", rnd.clone()).into()),
+        (
+            "t1".to_owned(),
+            Thermometer::new("Celsius", rnd.clone()).into(),
+        ),
+    ]));
 
-    //     rooms.push(Box::new(room1));
+    let mut smart_house = SmartHouse::new(
+        "The House That Jack Built",
+        HashMap::from([
+            ("Molly's chamber".to_owned(), room1),
+            ("Jack's room".to_owned(), room2),
+        ]),
+    );
 
-    //     let room2 = Room::new(
-    //         345,
-    //         [
-    //             Socket::new(123, rnd.clone()).into(),
-    //             Socket::new(345, rnd.clone()).into(),
-    //             Thermometer::new(678, rnd.clone()).into(),
-    //         ],
-    //     );
+    print_report(&mut smart_house);
 
-    //     rooms.push(Box::new(room2));
+    let socket = smart_house.get_device_mut("Molly's chamber", "s1").unwrap();
 
-    //     let mut smart_house = SmartHouse::new(123, rooms);
+    match socket {
+       SmartDevice::Socket(s)=> s.on(),
+       _ => panic!("Something is wrong with your code, it should be a socket")     
+    }
 
-    //     println!("{:}", smart_house);
+    print_report(&mut smart_house);
 
-    //     let room1 = smart_house.get_room_mut(0);
-    //     device_on(room1.get_device_mut(0));
-
-    //     let room2 = smart_house.get_room_mut(1);
-    //     device_on(room2.get_device_mut(0));
-    //     device_on(room2.get_device_mut(1));
-
-    //     println!("{:}", smart_house);
-    // }
-
-    // fn device_on<R: Rng>(device: &mut SmartDevice<R>) {
-    //     match device {
-    //         SmartDevice::Socket(socket) => socket.on(),
-    //         _ => panic!("Something wrong with your code, it should return Socket"),
-    //     }
-    // }
 }
